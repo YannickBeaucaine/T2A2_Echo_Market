14c004902c213d5ed587df4157387feef8dce86c
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-05-26 17:59:44 +1000

    updtaed listing form with new fields and log in requirements for adding and editing functions

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,16 +1,20 @@
 class ListingsController < ApplicationController
+    # only allows access to this page after after logging in
+    before_action :authenticate_user!, only: [:new, :edit]
+    # shows all listing available 
     def index
       @listings = Listing.all
     end
-
+    # method to create new items
     def new
+      @listing = Listing.new
 
     end
 
     def edit
         
     end
-
+    # method to show details of listing
     def show
         @listing = Listing.find(params[:id])
 
