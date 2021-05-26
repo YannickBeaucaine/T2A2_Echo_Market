ed211841b2d6b09c3884fedad32f94ec8fb93280
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-05-26 14:15:01 +1000

    extended controller features aded for listing page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,7 +2,17 @@ class ListingsController < ApplicationController
     def index
       @listings = Listing.all 
     end
+
     def new
+
+    end
+
+    def edit
+        
+    end
+
+    def show
+
     end
 
 end
