387a7c90a49bcd53522317e70056e0b093713a94
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-05-28 11:18:08 +1000

    Added edit and remove functionalities to buttons on show page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,7 @@
 class ListingsController < ApplicationController
     # only allows access to this page after after logging in
     before_action :authenticate_user!, only: [:new, :edit]
-    before_action:set_listing, only: [:show]
+    before_action:set_listing, only: [:show, :edit, :update, :destroy]
     # shows all listing available 
     def index
       @listings = Listing.all
@@ -20,9 +20,17 @@ class ListingsController < ApplicationController
             render :new
         end
     end
-     
+    # logic controller method to edit listed item 
     def edit
         
+    end
+    # logic controller to update listed item
+    def update
+       if @listing.update(listing_params)
+        redirect_to @listing
+       else
+        render :edit
+       end
     end
     # method to show details of listing activating Stripe
     def show
@@ -36,7 +44,7 @@ class ListingsController < ApplicationController
            description: @listing.description,
            currency: 'aud',
            quantity: '1',
-           images: [@listing.images.first.service_url]
+           images: @listing.images.empty? ? nil : [@listing.images.first.service_url]
          }],
          payment_intent_data: {
              metadata: {
@@ -48,6 +56,12 @@ class ListingsController < ApplicationController
          cancel_url: "#{root_url}listings"
          )
          @session_id = stripe_session.id
+    end
+    # method to delete listed item
+    def destroy
+        @listing.destroy
+        flash[:alert] = 'Your listing has been removed'
+        redirect_to listings_path
     end
     # adding layer of security with srong params to avoid malicious editing
     private
