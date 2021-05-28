f4de75593448f7dd99af9fabbd9176bca3e5ad33
YannickBeaucaine <yannick.beaucaine@gmail.com>
2021-05-28 16:34:27 +1000

    listing styling added card format

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,10 +1,10 @@
 class Listing < ApplicationRecord
-  CONDITION = %w(Mint NearMint Good Average Bad)
+  # CONDITION = %w(Mint NearMint Good Average Bad)
   belongs_to :user
   has_one :purchase, dependent: :destroy
   has_many_attached :images
   validates :name, :price, :status, presence: true
-  validates :item_condition, inclusion: { in: CONDITION }
+  validates :item_condition, inclusion: { in: :collection }
   enum status: { active: 1, inactive: 2, purchased: 3 }
   scope :active, -> { where(status: 1) }
   after_initialize :set_default_status
